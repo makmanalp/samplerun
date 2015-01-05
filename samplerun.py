0eb1af0fc092248bf7513afa120251eb3b49a410
Mali Akmanalp <mali@akmanalp.com>
2015-01-05 18:00:58 +0200

    Separate out image set code

--- samplerun.py
+++ samplerun.py
@@ -1,12 +1,8 @@
 import unittest
 import pytest
 import math
-import os
 
-import skimage.io
-
-import json
-import os.path
+from image_set import image_set
 
 
 class SampleRunMixin:
@@ -66,31 +62,6 @@ class DetectionLocationtest(SampleRunTestCase):
                           [(2, 2)], [(2, 2), (3, 5)], 10)
         self.assertRaises(AssertionError, self.assert2DPointsWithin,
                           [(2, 2)], [(2, 2), (3, 5)], 10)
-
-
-class ImageSet(object):
-
-    def __init__(self, path, extensions=(".png", ".jpg")):
-        self.path = os.path.abspath(path)
-        self.extensions = extensions
-        data = json.loads(open(os.path.join(path, "points.json")).read())
-        self.points = {k: v for k, v in data.iteritems() if k.endswith(extensions)}
-
-    def images(self):
-        for image_name, points in self.points.iteritems():
-            image_path = os.path.join(self.path, image_name)
-            image_array = skimage.io.imread(image_path)
-            yield image_array, points
-
-
-def load_folders():
-    folders = ["images", "pinkball"]
-    cwd = os.path.abspath(os.path.curdir)
-    return [os.path.join(cwd, f) for f in folders]
-
-@pytest.fixture(params=load_folders())
-def image_set(request):
-    return ImageSet(request.param)
 
 
 class Algorithm(object):
